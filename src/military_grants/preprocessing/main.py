14fbf8a0c10049973f6350a84057f540d4b70fc6
bubblbu <asura.enkhbayar@gmail.com>
2020-04-21 18:39:32 -0700

    add logging

--- src/military_grants/preprocessing/main.py
+++ src/military_grants/preprocessing/main.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from .merge_excel_files import run
+from military_grants.utils.logging import logger
 
 
 def run_all():
+    logger.info("=== Preprocessing")
+    logger.info("Merging all excel files")
     run()
