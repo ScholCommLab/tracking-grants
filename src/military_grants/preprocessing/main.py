39b37e0f4a5f32151b52ea4bb5afe0a5acfdd629
bubblbu <asura.enkhbayar@gmail.com>
2020-04-21 18:37:27 -0700

    refactored preprocessing

---
+++ src/military_grants/preprocessing/main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .merge_excel_files import run
+
+
+def run_all():
+    run()
---
+++ src/military_grants/preprocessing/merge_excel_files.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from pathlib import Path
+
+from military_grants import data_dir
+from military_grants import INPUT
+from military_grants.utils.logging import logger
+
+
+def process_excel(excel_files: list, input_f: Path):
+    """ Runs some preprocessing on input Excel in "data/external" to
+    de-duplicate exports it into two formats:
+        1. CSV for further processing
+        2. Text file with one citation per line for citation parsing
+    """
+    logger.debug("Load Excel spreadsheet and run some preprocessing.")
+
+    # Load excel files
+    dfs = []
+    for f in excel_files:
+        df = pd.read_excel(f, index_col=0)
+        df["type"] = f.name.split(".")[0]
+        dfs.append(df)
+
+    # Merge datasets
+    pubs = pd.concat(dfs)
+
+    # Drop duplicate entries
+    logger.debug(f"Duplicate entries in dataset: {sum(pubs.duplicated())}")
+    pubs = pubs.drop_duplicates()
+
+    # Reindex and rename columns
+    pubs.index = range(0, len(pubs))
+    pubs.index.name = "article_id"
+    pubs.columns = ["proposal_id", "unstruct_citation", "type"]
+
+    # Write unique references to files
+    logger.debug("Writing cleaned publication data to CSV")
+    pubs.to_csv(input_f)
+
+
+def run():
+    input_folder = data_dir / "external"
+    excel_files = input_folder.glob("*.xlsx")
+
+    input_f = data_dir / INPUT
+
+    if not Path(input_f).exists():
+        logger.info("Process Excel file")
+        process_excel(excel_files, input_f)
+    else:
+        logger.info("Skipping: Excel file has already been processed")
