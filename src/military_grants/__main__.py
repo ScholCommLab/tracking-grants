1073a07d400a307bc10f1f6714719a4ecd52772c
bubblbu <asura.enkhbayar@gmail.com>
2020-04-25 16:17:57 -0700

    add script to run all steps

---
+++ src/military_grants/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from military_grants.utils.logging import logger
+
+from .collect_metrics.main import run_all as metrics
+from .preprocessing.main import run_all as preprocessing
+from .process_articles.main import run_all as articles
+from .process_references.main import run_all as references
+
+
+def run():
+    logger.info("+++ Running all processing steps at once.")
+
+    preprocessing()
+    references()
+    articles()
+    metrics()
