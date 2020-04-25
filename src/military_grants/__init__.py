5a7740a1f68076793d84972f61265b4e372bef23
bubblbu <asura.enkhbayar@gmail.com>
2020-04-24 17:01:01 -0700

    defined all input/output files

--- src/military_grants/__init__.py
+++ src/military_grants/__init__.py
@@ -5,22 +5,25 @@ from pathlib import Path
 import seaborn as sns
 from dotenv import find_dotenv, load_dotenv
 
-# Default locations
-project_dir = Path(__file__).resolve().parents[2]
-data_dir = project_dir / "data"
-
 # Plotting style
 sns.set_style("darkgrid")
 sns.set(rc={"figure.figsize": (8, 5)})
 
-# find .env automagically by walking up directories until it's found, then
-# load up the .env entries as environment variables
-load_dotenv(find_dotenv())
-CR_THRESH = int(os.getenv("cr_thresh"))
+# Default locations
+project_dir = Path(__file__).resolve().parents[2]
+data_dir = project_dir / "data"
 
-# All constant files
-EXCEL = os.getenv("EXCEL")
-INPUT = os.getenv("INPUT")
-UNSTRUCTURED_REFS = os.getenv("UNSTRUCTURED_REFS")
-STRUCTURED_REFS = os.getenv("STRUCTURED_REFS")
-ARTICLES = os.getenv("ARTICLES")
+# External files
+EXCEL = "external/input/"
+
+# Temporary files
+REFERENCES = "interim/references.csv"
+ONE_REF_PER_LINE = "interim/one_ref_per_line.txt"
+CROSSREF = "interim/reference_match_output.json"
+ALTMETRIC = "interim/altmetric.csv"
+WOS = "interim/wos.csv"
+
+# Processed files
+ARTICLES = "processed/articles.csv"
+METRICS = "processed/metrics.csv"
+
