1660686b78a35221f593a515f5e0c73203198deb
bubblbu <asura.enkhbayar@gmail.com>
2020-04-27 22:03:27 -0700

    move all user settings and configurables to the .env.example file

--- src/military_grants/__init__.py
+++ src/military_grants/__init__.py
@@ -5,13 +5,16 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import seaborn as sns
 
+# Load configuration
+load_dotenv(find_dotenv())
+
 # Plotting style
 sns.set_style("darkgrid")
 sns.set(rc={"figure.figsize": (8, 5)})
 
-config = load_dotenv(find_dotenv())
-
+# Configure altmetric key
 altmetric_api_key = os.getenv("ALTMETRIC_API_KEY")
+
 # Altmetric call limit
 if not altmetric_api_key:
     altmetric_call_limit = 1
@@ -20,25 +23,23 @@ else:
 
 # Directories
 project_dir = Path(__file__).resolve().parents[2]
+data_dir = project_dir / "data"
+
+# Raw data from CDMRP
+input_folder = data_dir / os.getenv("INPUT_FOLDER")
+
+# Temporary files
+one_ref_per_line = data_dir / os.getenv("INPUT_FOLDER")
+crossref_f = data_dir / os.getenv("CROSSREF")
+altmetric_f = data_dir / os.getenv("ALTMETRIC")
+wos_f = data_dir / os.getenv("WOS")
+
+# Processed files
+references_f = data_dir / os.getenv("REFERENCES")
+articles_f = data_dir / os.getenv("ARTICLES")
+metrics_f = data_dir / os.getenv("METRICS")
 
 # External software
 reference_matcher = (
     project_dir / "crossref/search-based-ref-matching-1.1-jar-with-dependencies.jar"
 )
-
-# Data
-data_dir = project_dir / "data"
-
-# Raw data from CDMRP
-input_folder = data_dir / "raw"
-
-# Temporary files
-one_ref_per_line = data_dir / "interim/one_ref_per_line.txt"
-crossref_f = data_dir / "interim/reference_match_output.json"
-altmetric_f = data_dir / "interim/altmetric.json"
-wos_f = data_dir / "interim/wos.json"
-
-# Processed files
-references_f = data_dir / "processed/references.csv"
-articles_f = data_dir / "processed/articles.csv"
-metrics_f = data_dir / "processed/metrics.csv"
