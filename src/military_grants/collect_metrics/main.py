111b18f832eb5af1fdabe79e0f9fd08b9ab8d614
bubblbu <asura.enkhbayar@gmail.com>
2020-04-27 18:03:52 -0700

    remove wos processing. requires manual data collection

--- src/military_grants/collect_metrics/main.py
+++ src/military_grants/collect_metrics/main.py
@@ -2,7 +2,6 @@
 from military_grants.utils.logging import logger
 
 from .altmetrics import run as collect_altmetrics
-from .wos import run as collect_wos
 from .process_metrics import run as process_metrics
 
 
@@ -10,5 +9,4 @@ def run_all():
     logger.info("Processing all articles")
 
     collect_altmetrics()
-    collect_wos()
     process_metrics()
--- src/military_grants/collect_metrics/wos.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from military_grants.utils.logging import logger
-
-def run():
-    pass
