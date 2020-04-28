7ab921106b1a8dd25e3931cf3a6ee23fa9bc026b
bubblbu <asura.enkhbayar@gmail.com>
2020-04-27 18:02:45 -0700

    add first version for metrics processing

--- src/military_grants/collect_metrics/process_metrics.py
+++ src/military_grants/collect_metrics/process_metrics.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
+import json
+
+import pandas as pd
+from military_grants import altmetric_f, metrics_f, wos_f
 from military_grants.utils.logging import logger
-from military_grants import articles_f, altmetric_f, wos_f
 
 
-def process_metrics():
-    pass
+def process_metrics(altmetric_f, wos_f, metrics_f):
+    """Merge data from Altmetric and WoS."""
+    altmetric = pd.DataFrame.from_dict(json.loads(altmetric_f.read_text()), orient="columns").T
+    wos = pd.read_csv(wos_f, index_col="doi")
+
+    altmetric.join(wos).to_csv(metrics_f)
 
 
 def run():
-    pass
+    if not metrics_f.exists():
+        logger.info("\tProcessing collected altmetrics and data from the WoS.")
+        process_metrics(altmetric_f, wos_f, metrics_f)
+    else:
+        logger.info("\tSkipped: Metrics have already been processed and exported.")
