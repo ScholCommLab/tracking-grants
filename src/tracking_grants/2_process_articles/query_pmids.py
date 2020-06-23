cd4565e2c9ca4e2a90a2bc3439ca1b491a77f89d
bubblbu <asura.enkhbayar@gmail.com>
2020-06-23 00:36:03 -0700

    fix pmid queries

--- src/tracking_grants/2_process_articles/query_pmids.py
+++ src/tracking_grants/2_process_articles/query_pmids.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-import os
-import xml.etree.ElementTree as ET
+import re
 
 import pandas as pd
 import requests
+from ratelimit import limits, sleep_and_retry
 from tqdm.auto import tqdm
-
-from tracking_grants import articles_f
+from tracking_grants import articles_f, email, ncbi_api_key, tool_name
 from tracking_grants.utils.logging import logger
+
+
+tqdm.pandas()
 
 
 class Eutils:
@@ -20,38 +22,39 @@ class Eutils:
             "db": "pubmed",
             "retmax": 1,
         }
-
+    
+    @sleep_and_retry
+    @limits(calls=10, period=1)
     def search(self, doi):
         params = self.params
         params["term"] = doi
-        return requests.get(self.search_api, params=params)
+        
+        r = requests.get(self.search_api, params=params)
+        
+        # Only return DOI has been found in the text
+        if 'PhraseNotFound' not in r.text:
+            count = int(re.search(r"<Count>(\d+)<\/Count>", r.text).group(1))
+            # Only return unique matches
+            if count == 1:
+                match = re.search(r"<Id>(\d+)<\/Id>", r.text)
+                if match:
+                    return match.group(1)
+        return None
 
 
 def query_pmids(articles_f):
-    articles = pd.read_csv(articles_f, index_col="article_id")
+    articles = pd.read_csv(articles_f)
 
-    tool = os.getenv("NCBI_TOOL")
-    email = os.getenv("NCBI_EMAIL")
-    api_key = os.getenv("NCBI_API_KEY")
+    # Init search with user data
+    eutils = Eutils(tool_name, email, ncbi_api_key)
 
-    eutils = Eutils(tool, email, api_key)
-
-    for ix, row in tqdm(articles.iterrows(), total=len(articles)):
-        try:
-            resp = eutils.search(row["DOI"])
-        except Exception as e:
-            print(e)
-
-        root = ET.fromstring(resp.text)
-        pmids = root.findall("*/Id")
-        if pmids:
-            articles.loc[ix, "pmid"] = pmids[0].text
-
-    articles.to_csv(articles_f)
+    # Query PMIDs with DOIs
+    articles['pmid'] = articles.DOI.progress_map(eutils.search)
+    articles.to_csv(articles_f, index=False)
 
 
 def run():
-    articles = pd.read_csv(articles_f, index_col="article_id")
+    articles = pd.read_csv(articles_f, nrows=5)
 
     if "pmid" not in articles.columns:
         logger.info("\tEnriching articles with Pubmed IDs.")
